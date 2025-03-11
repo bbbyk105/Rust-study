006e2eba971adbd68bdc2432ee002b9a24eb1c93
Byakko Kondo <bbbyk105@yahoo.co.jp>
2025-03-11 20:58:13 +0900

    予想と比較

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,5 +1,9 @@
-use std::io;
+// use std::io;
+// use rand::Rng;
+// use
 use rand::Rng;
+use std::cmp::Ordering;
+use std::io;
 
 
 fn main() {
@@ -11,11 +15,22 @@ fn main() {
 
     println!("Please input your guess.");
 
+
     let mut guess = String::new();
 
     io::stdin()
         .read_line(&mut guess)
         .expect("Failed to read line");
+    
+    let guess: u32 = guess.trim().parse()
+        .expect("Please type the number!");
 
     println!("You guessed: {}", guess);
+
+    match guess.cmp(&secret_number) {
+        Ordering::Less => println!("Too small"),
+        Ordering::Greater => println!("Too big"),
+        Ordering::Equal => println!("You win!"),
+    }
+    
 }
