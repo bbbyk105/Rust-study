25ce7a2d86f0950ec7b91ee7d6d93b5ef42209a8
Byakko Kondo <bbbyk105@yahoo.co.jp>
2025-03-11 21:14:04 +0900

    数字当てゲーム完成

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -11,7 +11,8 @@ fn main() {
 
     let secret_number = rand::thread_rng().gen_range(1..101);
 
-    println!("The secret number is {}", secret_number);
+
+    loop {
 
     println!("Please input your guess.");
 
@@ -22,15 +23,26 @@ fn main() {
         .read_line(&mut guess)
         .expect("Failed to read line");
     
-    let guess: u32 = guess.trim().parse()
-        .expect("Please type the number!");
+    let guess: u32 = match guess.trim().parse(){
+        Ok(num) => num,
+        Err(_) => continue,
+    };
+
 
     println!("You guessed: {}", guess);
 
-    match guess.cmp(&secret_number) {
-        Ordering::Less => println!("Too small"),
-        Ordering::Greater => println!("Too big"),
-        Ordering::Equal => println!("You win!"),
+
+    
+        println!("Please input your guess.");
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small"),
+            Ordering::Greater => println!("Too big"),
+            Ordering::Equal => {
+                println!("You win!");
+                break
+            }
+        }
     }
     
 }
